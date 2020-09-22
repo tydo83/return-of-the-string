4e5b40bb19821e1d2a141dd7ac8ed88f318c3322
Timothy Do <tydo83@gmail.com>
2020-09-22 12:35:28 -0400

    little bit tough

--- main.js
+++ main.js
@@ -2,11 +2,34 @@
  * YOUR CODE HERE! *
  *******************/
 
+function yell(str){
+  return str + '!';
+}
+function getFirstCharacter(str){
+  return str[0];
+}
+function getLastCharacter(str){
+  return str[str.length-1];
+}
+function getOneCharacter(str, num){
+  return str[num];
+}
+function getTwoCharacters(str, num1, num2){
+  return str[num1] + str[num2];
+}
+function makeCapitalized(str){
+  return str.toUpperCase();
+}
 
-
-
-
-
+function yellLouder(str){
+  return makeCapitalized(makeCapitalized(str)) + '!!!';
+}
+function getInitials(str){
+  let indexSpace = str.indexOf(' ');
+  let secondInitial = str.charAt(indexSpace + 1); 
+  return str[0] + '.' + secondInitial + '.';
+  }
+  
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
  ********************************************************************/
