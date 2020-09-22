304abc217946344d2eb1858aab9432fa4cd1a99b
Colin Jaffe <balloonasaurus@gmail.com>
2020-09-22 02:59:22 -0400

    Removes unnecessary requirements and makes remaining requirements more robust.

--- main.test.js
+++ main.test.js
@@ -32,7 +32,7 @@ describe('getFirstCharacter', () => {
 
 describe('getLastCharacter', () => {
   it('returns only the last character of a string', () => {
-    expect(getLastCharacter('mcLasterton')).toBe('n');
+    expect(getLastCharacter('Mr. Lasterton')).toBe('n');
   })
 
   it('returns a space if the string ends with one', () => {
@@ -42,40 +42,44 @@ describe('getLastCharacter', () => {
 
 describe('getOneCharacter', () => {
   it('returns an arbitrary character', () => {
+    expect(getOneCharacter('hello there', 0)).toBe('h');
     expect(getOneCharacter('hello there', 8)).toBe('e');
+    expect(getOneCharacter('and good-bye', 8)).toBe('-');
   })
 })
 
 describe('getTwoCharacters', () => {
   it('returns arbitrary characters from the given string', () => {
     expect(getTwoCharacters('I want a bagel!', 2, 11)).toBe('wg')
+    expect(getTwoCharacters('With cream cheese and lox, of course.', 6, 12)).toBe('rh')
   })
 
 })
 
 describe('makeCapitalized', () => {
-  it('returns upper cased version of a string', () => {
+  it('returns upper-cased version of a string', () => {
     expect(makeCapitalized('Speed Racer Was Great'))
       .toBe('SPEED RACER WAS GREAT');
+    expect(makeCapitalized('what are you talking about'))
+      .toBe('WHAT ARE YOU TALKING ABOUT')
     });
 });
 
 describe('yellLouder', () => {
   it('adds three exclamation points and capitalizes', () => {
     expect(yellLouder('hey')).toBe('HEY!!!');
+    expect(yellLouder('sorry for yelling so much')).toBe('SORRY FOR YELLING SO MUCH!!!');
   })
 })
 
 describe('getInitials', () => {
   it('returns the initials of a full name', () => {
     expect(getInitials('Herman Melville')).toBe('H.M.')
+    expect(getInitials('Speed Racer')).toBe('S.R.')
   });
-
-  it('returns the first initial twice if given only one name', () => {
-    expect(getInitials('Colin')).toBe('C.C.');
-  })
 
   it('returns only two initials if given three names', () => {
     expect(getInitials('Colin Raymond Jaffe')).toBe('C.R.')
+    expect(getInitials('Neil Patrick Harris')).toBe('N.P.')
   })
 })
